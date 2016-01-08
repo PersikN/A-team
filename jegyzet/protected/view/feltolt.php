9dac9e2280a756761db8e0739f7233baeec8c47d
PersikN <persik.n93@gmail.com>
2016-01-08 22:04:31 +0100

    feltolt_végleges

--- jegyzet/protected/view/feltolt.php
+++ jegyzet/protected/view/feltolt.php
@@ -1,4 +1,4 @@
-<form name="feltoltes" action="protected/view/feltoltes.php" method="post" enctype="multipart/form-data">
+<form name="feltoltes" action="protected/action/feltoltes.php" method="post" enctype="multipart/form-data">
 <table border="0">
 <tr><td>Feltöltés neve:</td><td align="left"><input size="20" type="text" name="nev" id="nev"></td></tr>
 <tr><td>Feltöltendő fájl:</td><td align="left"><input type="file" name="fajl"></td></tr>
