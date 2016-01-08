8ee51d8b9236e98d1c65f4feb158b031a7929875
SBoglarka <bogisomogyi@gmail.com>
2016-01-08 22:08:19 +0100

    Módosítás felület

--- jegyzet/protected/view/modosit.php
+++ jegyzet/protected/view/modosit.php
@@ -1,8 +1,38 @@
 <?php
+	include_once './protected/mydbms.php';   
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+?>
+<form name="módosítás" action="protected/action/mod.php" method="post" enctype="multipart/form-data">
+<table border="0">
+<tr><td>Feltöltés neve:</td><td align="left"><input size="20" type="text" name="nev" id="nev"></td></tr>
+<tr><td>Feltöltendő fájl:</td><td align="left"><input type="file" name="fajl"></td></tr>
+<tr><td></td><td><input type="submit" value="Módosítás"></td></tr></table>
+</form>
+<form action="" method="POST">
+<table border="2">
+    <thead>
+        <tr>
+            <td>Név</td>
+            <td>Fájl</td>
+            <td>Módosítás</td>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+            $fajlok = fajlokLekerdez();
+            $fajlokDb = count($fajlok);
+            for($i = 0; $i < $fajlokDb;$i++){
+                echo '<tr>
+                     
+                        <td>'.$fajlok[$i]['nev'].'</td>
+                        <td>'.$fajlok[$i]['fajl'].'</td>
+                        <td><button type="submit" name="modosit" value="'.$fajlok[$i]['id'].'">Módosítás</button></td>    
+                     </tr>';
+            }
+        
+        ?>
+    </tbody>
+</table>
+</form>
+
 
