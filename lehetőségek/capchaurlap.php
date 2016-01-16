77ef7e9fd36940c90a18c299fa6678198f466a25
tittogetto <tittogetto@freemail.hu>
2016-01-16 21:06:47 +0100

    ellenőrző

---
+++ lehetőségek/capchaurlap.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<title>PHP CAPTCHA - Test</title>
+</head>
+<body>
+<form method="POST" action="capchaform.php">
+<table><tr><th colspan="3">PHP CAPTCHA - Tes</th></tr>
+<tr><td>Security code:</td><td><img src="mycapcha.php"><br></td>
+<td><input type="text" name="captcha_code" value="" maxlength="6"></td>
+</tr>
+<tr>
+<td colspan="3" style="text-align:left;">
+<input type="submit" name="send" value="Sending data">
+</td>
+</tr>
+</table>
+</form>
+</body>
+</html>
+
