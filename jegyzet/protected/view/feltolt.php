447149502bd50198d9692556f46740eea6eb0917
duslobonc <duslobonc@gmail.com>
2016-01-17 23:48:14 +0100

    kategória hozzáadása

--- jegyzet/protected/view/feltolt.php
+++ jegyzet/protected/view/feltolt.php
@@ -3,4 +3,6 @@
 <tr><td>Feltöltés neve:</td><td align="left"><input size="20" type="text" name="nev" id="nev"></td></tr>
 <tr><td>Feltöltendő fájl:</td><td align="left"><input type="file" name="fajl"></td></tr>
 <tr><td></td><td><input type="submit" value="Feltöltés"></td></tr></table>
+<tr> <td>Feltöltendő fájl tipusa:</td> <td align="left"> <select name="kategoria">
+<option value="1">Szöveg</option> <option value="2">Kép</option> </select> </td> </tr>
 </form>
