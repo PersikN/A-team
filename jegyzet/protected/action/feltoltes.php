35056315bc7a090c72749561b78971d4d62fe750
duslobonc <duslobonc@gmail.com>
2016-01-17 23:42:55 +0100

    feltöltés javítása(1),nem működött.

--- jegyzet/protected/action/feltoltes.php
+++ jegyzet/protected/action/feltoltes.php
@@ -6,10 +6,11 @@
 include_once "../mydbms.php";
 $safe_filename=trim($_FILES['fajl']['name']);  
 $safe_filename=rand().$safe_filename;
-if($feltoltes=feltoltes($_POST['nev'],"./public/uploads/".$safe_filename)){
-			move_uploaded_file($_FILES['fajl']['tmp_name'],"C:/wamp/www/jegyzet/public/uploads/".$safe_filename);
-			echo "A választott fájl sikeresen feltöltésre került!";
-        }
+if($feltoltes=feltoltes($_POST['nev'],"/public/uploads/".$safe_filename,
+ $_POST['kategoria'])) { move_uploaded_file($_FILES['fajl']['tmp_name'],
+ '../../public/uploads/'.$safe_filename);
+            echo "A választott fájl sikeresen feltöltésre került!"; }
+        
         else{
             echo "A választott fájl feltöltése sikertelen!";
         }
